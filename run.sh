e722ab1e3caa1506c143d7ffe5ce16a75f5283f3
Kamil Konior <kamil.koniorxcode@gmail.com>
2024-10-20 00:06:54 +0200

    configure creation of uber jar

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-# This file will be included as a Docker ENTRYPOINT in our automated testing evironment. 
-
-echo "Build and Run you server here"
-exit 1
+# This file will be included as a Docker ENTRYPOINT in our automated testing evironment.
+mvn clean package
+exec java -jar target/smart-home-1.0-SNAPSHOT.jar
